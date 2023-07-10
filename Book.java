bdced68f827443d6c4a9a5363104743d1fb3d380
anghazaryan <anahit.ghazaryan91@gmail.com>
2023-07-10 11:50:04 +0400

    updated

---
+++ Book.java
@@ -0,0 +1,12 @@
+public class Book extends Library{
+
+	private Author[] authors;
+
+	public Author[] getAuthors() {
+        return authors;
+    }
+    public void setAuthors(Author[] authors) {        
+            this.authors = authors;     
+          
+    }	
+}
--- Library.java
+++ Library.java
@@ -1,3 +1,19 @@
 public class Library{
-	
+
+	private Book[] books;
+	private Librarian[] librarians;
+
+
+	public Book[] getBooks(){
+		return books;
+	}
+	public void setBooks(Book[] books){
+		this.books = books;
+	}
+	public Librarian[] getLibrarians(){
+		return librarians;
+	}
+	public void setLibrarians(Librarian[] librarians){
+		this.librarians = librarians;
+	}
 }
